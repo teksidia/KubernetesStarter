6412f85c882c6b6767f30f9a4241dff3b7c9354e
Jamie Cox <owlface23@yahoo.co.uk>
2021-03-16 15:04:09 +0000

    Add project files.

---
+++ CommandsCheatsheet.md
@@ -0,0 +1,22 @@
+# Commands Cheatsheet
+
+## Docker
+
+```docker pull [image]```
+
+```docker run -it --name [name] [image]```
+
+```docker images```
+
+```docker ps```
+
+```docker tag [image] repo/image```
+
+```docker push [image]```
+
+
+
+
+
+
+
---
+++ StarterUi/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StarterUi.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<IndexModel> _logger;
+
+        public string ApiResult { get; set; }
+
+        public IndexModel(HttpClient httpClient, ILogger<IndexModel> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var result = await _httpClient.GetAsync("http://starterapi/weatherforecast");
+            ApiResult = await result.Content.ReadAsStringAsync();
+        }
+    }
+}
